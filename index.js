16d67e352fd05f691933873ac88cbe6cfaa6a0f0
shubhamAsolkar22 <38282971+shubhamAsolkar22@users.noreply.github.com>
2019-03-31 18:06:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var app = express();
+var things = require('./routes/things');
+var monkey = require('./routes/monkey');
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
+function middleware1(req,res,next){
+    console.log('pre-middleware1');
+    next();
+    console.log('post-middleware1');
+}
+
+function finalHandler(req,res,next){
+    console.log('finalhandler');
+    res.send("from final handler");
+}
+
+app.use('/things',things);
+app.use('/monkey',monkey);
+
+app.use('/middlewareDemo',middleware1,finalHandler);
 
 app.listen(3000);
